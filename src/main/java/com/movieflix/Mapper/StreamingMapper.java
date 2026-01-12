2fb474934d652672320cae5a9c29f2ec454cb691
Diego Micali <diego.micali13@gmail.com>
2026-01-12 12:02:57 -0300

    feat: implement streaming CRUD operations

---
+++ src/main/java/com/movieflix/Mapper/StreamingMapper.java
@@ -0,0 +1,25 @@
+package com.movieflix.Mapper;
+
+import com.movieflix.entity.Streaming;
+import com.movieflix.request.StreamingRequest;
+import com.movieflix.response.StreamingResponse;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class StreamingMapper {
+
+    public static Streaming toStreaming(StreamingRequest request) {
+        return Streaming
+                .builder()
+                .name(request.name())
+                .build();
+    }
+
+    public static StreamingResponse toStreamingResponse(Streaming streaming) {
+        return StreamingResponse
+                .builder()
+                .id(streaming.getId())
+                .name(streaming.getName())
+                .build();
+    }
+}
---
+++ src/main/java/com/movieflix/controller/StreamingController.java
@@ -0,0 +1,52 @@
+package com.movieflix.controller;
+
+import com.movieflix.Mapper.StreamingMapper;
+import com.movieflix.entity.Streaming;
+import com.movieflix.request.StreamingRequest;
+import com.movieflix.response.StreamingResponse;
+import com.movieflix.service.StreamingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movieflix/streaming")
+@RequiredArgsConstructor
+public class StreamingController {
+
+    private final StreamingService streamingService;
+
+    @GetMapping()
+    public ResponseEntity<List<StreamingResponse>> getAll() {
+        List<StreamingResponse> streamings = streamingService.findAll()
+                .stream()
+                .map(streaming -> StreamingMapper.toStreamingResponse(streaming))
+                .toList();
+        return ResponseEntity.ok(streamings);
+
+    }
+
+    @PostMapping
+    public ResponseEntity<StreamingResponse> save(@RequestBody StreamingRequest request) {
+        Streaming newStreaming = StreamingMapper.toStreaming(request);
+        Streaming savedStreaming = streamingService.save(newStreaming);
+        return ResponseEntity.status(HttpStatus.CREATED).body(StreamingMapper.toStreamingResponse(savedStreaming));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<StreamingResponse> getById(@PathVariable Long id) {
+        return streamingService.findById(id)
+                .map(streaming -> ResponseEntity.ok(StreamingMapper.toStreamingResponse(streaming)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        streamingService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}
+
